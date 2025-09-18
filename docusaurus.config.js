92ccf05f59b55f7131815ab08bf95cb334a87380
sakurawald <sakurawald@gmail.com>
2025-09-18 16:39:23 +0800

    fix: the org name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'fuji', // Usually your repo name.
+    organizationName: 'fuji-fabric', // Usually your GitHub org/user name.
+    projectName: 'docs-site', // Usually your repo name.
 
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
